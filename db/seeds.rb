91a199bede4038ddcf7fba80115b5be927b4e017
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-09-29 16:37:39 +0900

    db/seeds.rbに初期投稿データを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+if Post.count.zero?
+	Post.create!(
+		[
+			{ title: '最初の投稿', content: 'これはサンプルの内容です。' },
+			{ title: 'Rails API勉強', content: 'CRUD操作を練習しましょう。' }
+		]
+	)
+	puts 'Seed: posts created'
+end
